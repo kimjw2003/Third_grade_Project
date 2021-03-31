a7a8445620a787984210b24b039228a69d1e273a
kimjw2003 <kimjw111003@gmail.com>
2021-03-31 13:57:37 +0900

    [ADD] Fragments, Activities

---
+++ app/src/main/java/com/example/third_grade_project/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.third_grade_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
---
+++ app/src/main/java/com/example/third_grade_project/ui/list/ChoiceActivity.kt
@@ -0,0 +1,12 @@
+package com.example.third_grade_project.ui.list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.third_grade_project.R
+
+class ChoiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choice)
+    }
+}
