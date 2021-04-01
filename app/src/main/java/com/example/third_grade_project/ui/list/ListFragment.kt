fac7b70e422446bc7bce75a407318bde86885a6d
kimjw2003 <kimjw111003@gmail.com>
2021-04-01 12:17:46 +0900

    [ADD] basic layouts

--- app/src/main/java/com/example/third_grade_project/ui/list/ListFragment.kt
+++ app/src/main/java/com/example/third_grade_project/ui/list/ListFragment.kt
@@ -1,18 +1,29 @@
 package com.example.third_grade_project.ui.list
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.third_grade_project.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        writing_fabBtn.setOnClickListener {
+            val intent = Intent(activity, ChoiceActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
-
 
     }
 }
